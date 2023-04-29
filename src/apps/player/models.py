c707dc52c40bcd07e9159c6b0449092ef938ffd3
xpfxz <xpfxz@bk.ru>
2023-04-29 14:51:11 +0300

    new

--- src/apps/player/models.py
+++ src/apps/player/models.py
@@ -2,8 +2,27 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class TrackList(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+    duration = models.IntegerField()
+    localion = models.CharField(max_length=100)
+
+
 class CustomUser(AbstractUser):
-    pass
+    id = models.IntegerField(primary_key=True)
+    last_name = None
+    first_name = None
+    last_login = None
+    date_joined = None
+    email = models.EmailField(max_length=60)
+    track = models.ManyToManyField(TrackList, through='UserMusic')
 
     def __str__(self):
         return self.username
+
+
+class UserMusic(models.Model):
+    id = models.IntegerField(primary_key=True)
+    track = models.ForeignKey(TrackList, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
